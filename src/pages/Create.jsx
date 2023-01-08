9514ac61f5207435267132a88c50f4b993909981
Jonathan <jonato96s@gmail.com>
2023-01-08 14:25:38 -0500

    Saving states

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,42 @@
-const Create = () =>{
-    return(
+import { useState } from "react"
+
+const Create = () => {
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("");
+    const [cover, setCover] = useState("");
+    const [intro, setIntro] = useState("");
+    const [completed, setCompleted] = useState(false);
+    const [review, setReview] = useState("");
+
+    function handleChange(e) {
+        const name = e.target.name
+        const value = e.target.value
+        switch (name) {
+            case "title":
+                setTitle(value);
+                break;
+            case "author":
+                setAuthor(value);
+                break;
+            case "intro":
+                setIntro(value);
+                break;
+            case "completed":
+                setCompleted(value);
+                break;
+            case "review":
+                setReview(value);
+                break;
+        }
+    }
+    return (
         <div>
-            Create
+            <form>
+                <div>
+                    <div>Title</div>
+                    <input name="title" onChange={handleChange} value={title} type="text" />
+                </div>
+            </form>
         </div>
     )
 }
